c58645bb23cbee6227cd9d906be37be9d1a74042
Noban Hasan <nobanhasan@gmail.com>
2019-05-02 00:52:21 +0600

    Rx fixed

--- app/src/main/java/com/myres/noban/mvvmrxjava/viewModel/MainViewModel.kt
+++ app/src/main/java/com/myres/noban/mvvmrxjava/viewModel/MainViewModel.kt
@@ -1,54 +1,54 @@
 package com.myres.noban.mvvmrxjava.viewModel
 
 import android.app.Application
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.myres.noban.mvvmrxjava.actions.MainActivityAction
 import com.myres.noban.mvvmrxjava.model.Posts
 import com.myres.noban.mvvmrxjava.repository.PostRepository
+import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
 import java.util.function.Consumer
 
 class MainViewModel(
-    application: Application,
-    val mainActivityAction: MainActivityAction
+    application: Application
 ) : AndroidViewModel(application) {
 
     private var postRepository: PostRepository = PostRepository(application)
     private val compositeDisposable = CompositeDisposable()
 
     var isLoading = ObservableField<Boolean>()
-    var posts = MutableLiveData<ArrayList<Posts>>()
+    var posts = MutableLiveData<List<Posts>>()
 
     fun loadRepositories() {
-        isLoading.set(true)
-
-        compositeDisposable.add(
-            postRepository.getRepositories()
+       // isLoading.set(true)
+        Log.e("Call",":Ok")
+        val disposable:Disposable?=   postRepository.getRepositories()
                 ?.subscribeOn(Schedulers.io())
                 ?.observeOn(AndroidSchedulers.mainThread())
-                ?.subscribeWith(object : DisposableObserver<ArrayList<Posts>>() {
-                    override fun onNext(data: ArrayList<Posts>) {
-                        posts.value = data
-                    }
+                ?.subscribe(this::OnSuccess,this::onError)
+        if (disposable != null) {
+            compositeDisposable.add(disposable)
+        }
 
-                    override fun onError(e: Throwable) {
-                        mainActivityAction.showErrorInfo(e.message.toString())
-                    }
-
-                    override fun onComplete() {
-                        isLoading.set(false)
-                    }
-
-                })
-        )
     }
-
+    fun OnSuccess(posts : List<Posts>?){
+        if (posts != null) {
+            Log.e("Success", posts.size.toString()+" Data loaded")
+        }
+        this.posts.value= posts;
+    }
+    private fun onError(throwable: Throwable) {
+        Log.e("Success",":Error")
+    }
     override fun onCleared() {
         super.onCleared()
 
--- app/src/main/java/com/myres/noban/mvvmrxjava/views/MainActivity.kt
+++ app/src/main/java/com/myres/noban/mvvmrxjava/views/MainActivity.kt
@@ -2,11 +2,49 @@ package com.myres.noban.mvvmrxjava.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.myres.noban.mvvmrxjava.R
+import com.myres.noban.mvvmrxjava.adapter.PostRecyclerViewAdapter
+import com.myres.noban.mvvmrxjava.databinding.ActivityMainBinding
+import com.myres.noban.mvvmrxjava.model.Posts
+import com.myres.noban.mvvmrxjava.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
+    private val repositoryRecyclerViewAdapter = PostRecyclerViewAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProviders.of(
+            this
+        ).get(MainViewModel::class.java)
+
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main)
+
+        binding.apply {
+            viewModel = mainViewModel
+            executePendingBindings()
+            repositoryRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+            repositoryRecyclerView.adapter = repositoryRecyclerViewAdapter
+        }
+
+        mainViewModel.posts.observe(this, Observer<List<Posts>> {
+            it?.let {
+                repositoryRecyclerViewAdapter.notifyDataSetChanged()
+            }
+        })
+
     }
+
+
 }
