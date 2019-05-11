775a850971b8eb8465f00eddefeecef7de00a4e3
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2019-05-11 15:45:33 +0600

    Some issue in Dagger 2

--- app/src/main/java/com/myres/noban/mvvmrxjava/data/remote/GetPostFromRemoteServer.kt
+++ app/src/main/java/com/myres/noban/mvvmrxjava/data/remote/GetPostFromRemoteServer.kt
@@ -1,14 +1,13 @@
 package com.myres.noban.mvvmrxjava.data.remote
 
-import com.myres.noban.mvvmrxjava.factory.PostApiFactory
+import com.myres.noban.mvvmrxjava.di.component.DaggerPostApiServiceComponent
 import com.myres.noban.mvvmrxjava.model.Posts
-import com.myres.noban.mvvmrxjava.service.PostApiService
 import io.reactivex.Flowable
 
 class GetPostFromRemoteServer {
 
     fun getPosts(): Flowable<List<Posts>>? {
-        return PostApiFactory.getPostApiService(PostApiService::class.java)?.getAllPost()
+        return DaggerPostApiServiceComponent.builder().build().getPostApiService().getAllPost()
     }
-
 }
+
